ecc961c61cd3f23ce953b5ddefb015193d52368d
Tiago Anacretto <tiago.anacretto@gmail.com>
2021-11-22 00:13:14 -0300

    Preparação da parametrização de variaveis

--- src/main/java/br/com/sica/sicaativosservice/service/AuthorizationService.java
+++ src/main/java/br/com/sica/sicaativosservice/service/AuthorizationService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value="authorization", url="http://localhost:8081/auth")
+@FeignClient(value="authorization", url="${auth.url}")
 public interface AuthorizationService {
 
     @RequestMapping(method = RequestMethod.GET,
